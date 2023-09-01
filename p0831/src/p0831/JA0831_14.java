b410019266a204706d2ef95c7cae5a9e4c86d99f
KOREAVC <KOREAVC@DESKTOP-AI48KSJ>
2023-09-01 17:39:58 +0900

    9월 1일 - 연산자, 조건문

--- p0831/src/p0831/JA0831_14.java
+++ p0831/src/p0831/JA0831_14.java
@@ -10,12 +10,12 @@ public class JA0831_14 {
 		
 		int x = 1000000;
 		int y = 2000000;
-		long z = (long)x * y;  // long*int, long*long = long
+		long z = (long)x*y;  // long*int, long*long = long
 		System.out.println(z); 
 		
-		long num = (long)a * x / y;
+		long num = (long)a*x/y;
 		System.out.println(num);
-		long num2 = (long)a / x * y;
+		long num2 = (long)a/x*y;
 		System.out.println(num2);
 		
 		int i = 1;
